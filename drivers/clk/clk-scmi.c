1c27bfb8e7eb9ab39a5de15b0a00c4dbaf37e0ff
Cristian Marussi <cristian.marussi@arm.com>
2024-03-25 21:00:22 +0000

    clk: scmi: Add support for state control restricted clocks
    Some exposed SCMI Clocks could be marked as non-supporting state changes.
    Configure a clk_ops descriptor which does not provide the state change
    callbacks for such clocks when registering with CLK framework.
    
    CC: Michael Turquette <mturquette@baylibre.com>
    CC: Stephen Boyd <sboyd@kernel.org>
    CC: linux-clk@vger.kernel.org
    Signed-off-by: Cristian Marussi <cristian.marussi@arm.com>
    Link: https://lore.kernel.org/r/20240325210025.1448717-3-cristian.marussi@arm.com
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/clk/clk-scmi.c
+++ drivers/clk/clk-scmi.c
@@ -18,6 +18,7 @@
 
 enum scmi_clk_feats {
 	SCMI_CLK_ATOMIC_SUPPORTED,
+	SCMI_CLK_STATE_CTRL_FORBIDDEN,
 	SCMI_CLK_MAX_FEATS
 };
 
@@ -230,14 +231,18 @@ scmi_clk_ops_alloc(struct device *dev, unsigned long feats_key)
 	 * only the prepare/unprepare API, as allowed by the clock framework
 	 * when atomic calls are not available.
 	 */
-	if (feats_key & BIT(SCMI_CLK_ATOMIC_SUPPORTED)) {
-		ops->enable = scmi_clk_atomic_enable;
-		ops->disable = scmi_clk_atomic_disable;
-		ops->is_enabled = scmi_clk_atomic_is_enabled;
-	} else {
-		ops->prepare = scmi_clk_enable;
-		ops->unprepare = scmi_clk_disable;
+	if (!(feats_key & BIT(SCMI_CLK_STATE_CTRL_FORBIDDEN))) {
+		if (feats_key & BIT(SCMI_CLK_ATOMIC_SUPPORTED)) {
+			ops->enable = scmi_clk_atomic_enable;
+			ops->disable = scmi_clk_atomic_disable;
+		} else {
+			ops->prepare = scmi_clk_enable;
+			ops->unprepare = scmi_clk_disable;
+		}
 	}
+
+	if (feats_key & BIT(SCMI_CLK_ATOMIC_SUPPORTED))
+		ops->is_enabled = scmi_clk_atomic_is_enabled;
 
 	/* Rate ops */
 	ops->recalc_rate = scmi_clk_recalc_rate;
@@ -287,6 +292,9 @@ scmi_clk_ops_select(struct scmi_clk *sclk, bool atomic_capable,
 	 */
 	if (atomic_capable && ci->enable_latency <= atomic_threshold)
 		feats_key |= BIT(SCMI_CLK_ATOMIC_SUPPORTED);
+
+	if (ci->state_ctrl_forbidden)
+		feats_key |= BIT(SCMI_CLK_STATE_CTRL_FORBIDDEN);
 
 	/* Lookup previously allocated ops */
 	ops = clk_ops_db[feats_key];
