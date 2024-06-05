koverstreet/bcachefs

bcachefs: bch_acct_btree
Add counters for how much disk space we're using per btree.

Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

fs/bcachefs/buckets.c
@@ -595,6 +595,16 @@ static int __trigger_extent(struct btree_trans *trans,
 			return ret;
 	}
 
+	if (level) {
+		struct disk_accounting_pos acc_btree_key = {
+			.type		= BCH_DISK_ACCOUNTING_btree,
+			.btree.id	= btree_id,
+		};
+		ret = bch2_disk_accounting_mod(trans, &acc_btree_key, &dirty_sectors, 1, gc);
+		if (ret)
+			return ret;
+	}
+
 	return 0;
 }
 

ColinIanKing/linux-next

fs/bcachefs/buckets.c
@@ -760,6 +760,16 @@ static int __trigger_extent(struct btree_trans *trans,
 			return ret;
 	}
 
+	if (level) {
+		struct disk_accounting_pos acc_btree_key = {
+			.type		= BCH_DISK_ACCOUNTING_btree,
+			.btree.id	= btree_id,
+		};
+		ret = bch2_disk_accounting_mod(trans, &acc_btree_key, &replicas_sectors, 1, gc);
+		if (ret)
+			return ret;
+	}
+
 	return 0;
 }
 