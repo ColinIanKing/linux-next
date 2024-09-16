b70d20510656ae4e5c6fbd3c00350561a5f5b060
Pali Rohár <pali@kernel.org>
2024-09-16 20:21:52 +0200

    cifs: Add support for creating native Windows sockets
    Native Windows sockets created by WinSock on Windows 10 April 2018 Update
    (version 1803) or Windows Server 2019 (version 1809) or later versions is
    reparse point with IO_REPARSE_TAG_AF_UNIX tag, with empty reparse point
    data buffer and without any EAs.
    
    Create AF_UNIX sockets in this native format if -o nonativesocket was not
    specified.
    
    This change makes AF_UNIX sockets created by Linux CIFS client compatible
    with AF_UNIX sockets created by Windows applications on NTFS volumes.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/client/fs_context.c
+++ fs/smb/client/fs_context.c
@@ -185,6 +185,7 @@ const struct fs_parameter_spec smb3_fs_parameters[] = {
 	fsparam_string("cache", Opt_cache),
 	fsparam_string("reparse", Opt_reparse),
 	fsparam_string("upcall_target", Opt_upcalltarget),
+	fsparam_string("nativesocket", Opt_nativesocket),
 	fsparam_string("symlink", Opt_symlink),
 	fsparam_string("symlinkroot", Opt_symlinkroot),
 
@@ -1784,6 +1785,9 @@ static int smb3_fs_context_parse_param(struct fs_context *fc,
 		if (parse_reparse_flavor(fc, param->string, ctx))
 			goto cifs_parse_mount_err;
 		break;
+	case Opt_nativesocket:
+		ctx->nonativesocket = result.negated;
+		break;
 	case Opt_symlink:
 		if (parse_symlink_flavor(fc, param->string, ctx))
 			goto cifs_parse_mount_err;
@@ -1918,6 +1922,7 @@ int smb3_init_fs_context(struct fs_context *fc)
 	ctx->retrans = 1;
 	ctx->reparse_type = CIFS_REPARSE_TYPE_DEFAULT;
 	ctx->symlink_type = CIFS_SYMLINK_TYPE_DEFAULT;
+	ctx->nonativesocket = 0;
 
 /*
  *	short int override_uid = -1;
