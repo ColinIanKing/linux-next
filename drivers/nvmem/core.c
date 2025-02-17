11ccaa3121119eeff9ab9d537e0cf6be3b10698b
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2025-02-17 18:33:20 +0200

    nvmem: core: verify cell's raw_len
    Check that the NVMEM cell's raw_len is a aligned to word_size. Otherwise
    Otherwise drivers might face incomplete read while accessing the last
    part of the NVMEM cell.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- drivers/nvmem/core.c
+++ drivers/nvmem/core.c
@@ -605,6 +605,14 @@ static int nvmem_cell_info_to_nvmem_cell_entry_nodup(struct nvmem_device *nvmem,
 		return -EINVAL;
 	}
 
+	if (!IS_ALIGNED(cell->raw_len, nvmem->word_size)) {
+		dev_err(&nvmem->dev,
+			"cell %s raw len %zd unaligned to nvmem word size %d\n",
+			cell->name ?: "<unknown>", cell->raw_len,
+			nvmem->word_size);
+		return -EINVAL;
+	}
+
 	return 0;
 }
 
