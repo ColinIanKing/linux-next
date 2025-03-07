804294d75ac5743d194d252aaf298b718566d067
Brett Creeley <brett.creeley@amd.com>
2025-03-07 10:53:28 -0800

    pds_fwctl: add rpc and query support
    The pds_fwctl driver doesn't know what RPC operations are available
    in the firmware, so also doesn't know what scope they might have.  The
    userland utility supplies the firmware "endpoint" and "operation" id values
    and this driver queries the firmware for endpoints and their available
    operations.  The operation descriptions include the scope information
    which the driver uses for scope testing.
    
    Link: https://patch.msgid.link/r/20250307185329.35034-6-shannon.nelson@amd.com
    Reviewed-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Brett Creeley <brett.creeley@amd.com>
    Signed-off-by: Shannon Nelson <shannon.nelson@amd.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- include/uapi/fwctl/pds.h
+++ include/uapi/fwctl/pds.h
@@ -23,4 +23,20 @@ enum pds_fwctl_capabilities {
 	PDS_FWCTL_QUERY_CAP = 0,
 	PDS_FWCTL_SEND_CAP,
 };
+
+struct fwctl_rpc_pds {
+	struct {
+		__u32 op;
+		__u32 ep;
+		__u32 rsvd;
+		__u32 len;
+		__aligned_u64 payload;
+	} in;
+	struct {
+		__u32 retval;
+		__u32 rsvd[2];
+		__u32 len;
+		__aligned_u64 payload;
+	} out;
+};
 #endif /* _UAPI_FWCTL_PDS_H_ */
