43012aa3445455eb552fb9bfc2a283c5dcc5248e
Mickaël Salaün <mic@digikod.net>
2025-03-08 19:44:00 +0100

    landlock: Prepare to use credential instead of domain for filesystem
    This cosmetic change is needed for audit support, specifically to be
    able to filter according to cross-execution boundaries.
    
    Add landlock_get_applicable_subject(), mainly a copy of
    landlock_get_applicable_domain(), which will fully replace it in a
    following commit.
    
    Optimize current_check_access_path() to only handle the access request.
    
    Partially replace get_current_fs_domain() with explicit calls to
    landlock_get_applicable_subject().  The remaining ones will follow with
    more changes.
    
    Remove explicit domain->num_layers check which is now part of the
    landlock_get_applicable_subject() call.
    
    Cc: Günther Noack <gnoack@google.com>
    Link: https://lore.kernel.org/r/20250308184422.2159360-5-mic@digikod.net
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- security/landlock/cred.h
+++ security/landlock/cred.h
@@ -13,6 +13,7 @@
 #include <linux/init.h>
 #include <linux/rcupdate.h>
 
+#include "access.h"
 #include "ruleset.h"
 #include "setup.h"
 
@@ -51,6 +52,55 @@ static inline bool landlocked(const struct task_struct *const task)
 	has_dom = !!landlock_get_task_domain(task);
 	rcu_read_unlock();
 	return has_dom;
+}
+
+/**
+ * landlock_get_applicable_subject - Return the subject's Landlock credential
+ *                                   if its enforced domain applies to (i.e.
+ *                                   handles) at least one of the access rights
+ *                                   specified in @masks
+ *
+ * @cred: credential
+ * @masks: access masks
+ * @handle_layer: returned youngest layer handling a subset of @masks.  Not set
+ *                if the function returns NULL.
+ *
+ * Returns: landlock_cred(@cred) if any access rights specified in @masks is
+ * handled, or NULL otherwise.
+ */
+static inline const struct landlock_cred_security *
+landlock_get_applicable_subject(const struct cred *const cred,
+				const struct access_masks masks,
+				size_t *const handle_layer)
+{
+	const union access_masks_all masks_all = {
+		.masks = masks,
+	};
+	const struct landlock_ruleset *domain;
+	ssize_t layer_level;
+
+	if (!cred)
+		return NULL;
+
+	domain = landlock_cred(cred)->domain;
+	if (!domain)
+		return NULL;
+
+	for (layer_level = domain->num_layers - 1; layer_level >= 0;
+	     layer_level--) {
+		union access_masks_all layer = {
+			.masks = domain->access_masks[layer_level],
+		};
+
+		if (layer.all & masks_all.all) {
+			if (handle_layer)
+				*handle_layer = layer_level;
+
+			return landlock_cred(cred);
+		}
+	}
+
+	return NULL;
 }
 
 __init void landlock_add_cred_hooks(void);
