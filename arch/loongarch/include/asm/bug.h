c98a25ae14ab444f8248ba3d52765e2e2588c162
Guenter Roeck <linux@roeck-us.net>
2025-03-13 11:43:23 +0000

    loongarch: add support for suppressing warning backtraces
    Add name of functions triggering warning backtraces to the __bug_table
    object section to enable support for suppressing WARNING backtraces.
    
    To limit image size impact, the pointer to the function name is only added
    to the __bug_table section if both CONFIG_KUNIT_SUPPRESS_BACKTRACE and
    CONFIG_DEBUG_BUGVERBOSE are enabled.  Otherwise, the __func__ assembly
    parameter is replaced with a (dummy) NULL parameter to avoid an image size
    increase due to unused __func__ entries (this is necessary because
    __func__ is not a define but a virtual variable).
    
    Link: https://lkml.kernel.org/r/20250313114329.284104-9-acarmina@redhat.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Alessandro Carminati <acarmina@redhat.com>
    Tested-by: Linux Kernel Functional Testing <lkft@linaro.org>
    Acked-by: Dan Carpenter <dan.carpenter@linaro.org>
    Acked-by: David Gow <davidgow@google.com>
    Cc: Huacai Chen <chenhuacai@kernel.org>
    Cc: Albert Ou <aou@eecs.berkeley.edu>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Arthur Grillo <arthurgrillo@riseup.net>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brendan Higgins <brendan.higgins@linux.dev>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Charlie Jenkins <charlie@rivosinc.com>
    Cc: Daniel Diaz <daniel.diaz@linaro.org>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Dave Airlie <airlied@gmail.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Heiko Carstens <hca@linux.ibm.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Maíra Canal <mcanal@igalia.com>
    Cc: Maxime Ripard <mripard@kernel.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Naresh Kamboju <naresh.kamboju@linaro.org>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Paul Walmsley <paul.walmsley@sifive.com>
    Cc: Rae Moar <rmoar@google.com>
    Cc: Rich Felker <dalias@libc.org>
    Cc: Simon Horman <horms@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Thomas Zimemrmann <tzimmermann@suse.de>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/loongarch/include/asm/bug.h
+++ arch/loongarch/include/asm/bug.h
@@ -3,49 +3,65 @@
 #define __ASM_BUG_H
 
 #include <asm/break.h>
+#include <kunit/bug.h>
 #include <linux/stringify.h>
 #include <linux/objtool.h>
 
 #ifndef CONFIG_DEBUG_BUGVERBOSE
-#define _BUGVERBOSE_LOCATION(file, line)
+#define _BUGVERBOSE_LOCATION(file, func, line)
 #else
-#define __BUGVERBOSE_LOCATION(file, line)			\
+#ifdef CONFIG_KUNIT_SUPPRESS_BACKTRACE
+# define HAVE_BUG_FUNCTION
+# define __BUG_FUNC_PTR(func)  .long func - .;
+#else
+# define __BUG_FUNC_PTR(func)
+#endif /* CONFIG_KUNIT_SUPPRESS_BACKTRACE */
+
+#define __BUGVERBOSE_LOCATION(file, func, line)			\
 		.pushsection .rodata.str, "aMS", @progbits, 1;	\
 	10002:	.string file;					\
 		.popsection;					\
 								\
 		.long 10002b - .;				\
+		__BUG_FUNC_PTR(func)				\
 		.short line;
-#define _BUGVERBOSE_LOCATION(file, line) __BUGVERBOSE_LOCATION(file, line)
+#define _BUGVERBOSE_LOCATION(file, func, line) __BUGVERBOSE_LOCATION(file, func, line)
 #endif
 
 #ifndef CONFIG_GENERIC_BUG
-#define __BUG_ENTRY(flags)
+#define __BUG_ENTRY(flags, func)
 #else
-#define __BUG_ENTRY(flags) 					\
+#define __BUG_ENTRY(flags, func)				\
 		.pushsection __bug_table, "aw";			\
 		.align 2;					\
 	10000:	.long 10001f - .;				\
-		_BUGVERBOSE_LOCATION(__FILE__, __LINE__)	\
+		_BUGVERBOSE_LOCATION(__FILE__, func, __LINE__)	\
 		.short flags; 					\
 		.popsection;					\
 	10001:
 #endif
 
-#define ASM_BUG_FLAGS(flags)					\
-	__BUG_ENTRY(flags)					\
+#define ASM_BUG_FLAGS(flags, func)				\
+	__BUG_ENTRY(flags, func)				\
 	break		BRK_BUG;
 
-#define ASM_BUG()	ASM_BUG_FLAGS(0)
+#define ASM_BUG()	ASM_BUG_FLAGS(0, .)
 
-#define __BUG_FLAGS(flags, extra)					\
-	asm_inline volatile (__stringify(ASM_BUG_FLAGS(flags))		\
-			     extra);
+#ifdef HAVE_BUG_FUNCTION
+# define __BUG_FUNC    __func__
+#else
+# define __BUG_FUNC    NULL
+#endif
+
+#define __BUG_FLAGS(flags, extra)				\
+	asm_inline volatile (__stringify(ASM_BUG_FLAGS(flags, %0)) \
+			     extra : : "i" (__BUG_FUNC) );
 
 #define __WARN_FLAGS(flags)					\
 do {								\
 	instrumentation_begin();				\
-	__BUG_FLAGS(BUGFLAG_WARNING|(flags), ANNOTATE_REACHABLE(10001b));\
+	if (!KUNIT_IS_SUPPRESSED_WARNING(__func__))			\
+		__BUG_FLAGS(BUGFLAG_WARNING|(flags), ANNOTATE_REACHABLE(10001b));\
 	instrumentation_end();					\
 } while (0)
 
