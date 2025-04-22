badc66c78e94f434a59b1bad2c68e6006e31b515
Yangtao Li <frank.li@vivo.com>
2025-04-22 02:14:57 -0600

    btrfs: update insert_block_entry to to use rb helper
    Update insert_block_entry() to use rb_find_add().
    
    Suggested-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Yangtao Li <frank.li@vivo.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ref-verify.c
+++ fs/btrfs/ref-verify.c
@@ -89,27 +89,21 @@ static int block_entry_key_cmp(const void *k, const struct rb_node *node)
 	return 0;
 }
 
+static int block_entry_cmp(struct rb_node *new, const struct rb_node *exist)
+{
+	const struct block_entry *new_entry =
+		rb_entry(new, struct block_entry, node);
+
+	return block_entry_key_cmp(&new_entry->bytenr, exist);
+}
+
 static struct block_entry *insert_block_entry(struct rb_root *root,
 					      struct block_entry *be)
 {
-	struct rb_node **p = &root->rb_node;
-	struct rb_node *parent_node = NULL;
-	struct block_entry *entry;
+	struct rb_node *exist;
 
-	while (*p) {
-		parent_node = *p;
-		entry = rb_entry(parent_node, struct block_entry, node);
-		if (entry->bytenr > be->bytenr)
-			p = &(*p)->rb_left;
-		else if (entry->bytenr < be->bytenr)
-			p = &(*p)->rb_right;
-		else
-			return entry;
-	}
-
-	rb_link_node(&be->node, parent_node, p);
-	rb_insert_color(&be->node, root);
-	return NULL;
+	exist = rb_find_add(&be->node, root, block_entry_cmp);
+	return rb_entry_safe(exist, struct block_entry, node);
 }
 
 static struct block_entry *lookup_block_entry(struct rb_root *root, u64 bytenr)
