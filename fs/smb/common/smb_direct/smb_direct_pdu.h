41da4000974413b2bf86a7fcaae2f38be3975f89
Stefan Metzmacher <metze@samba.org>
2025-05-27 18:13:00 +0200

    smb: common: add smb_direct_buffer_descriptor_v1
    Will be used in client and server in the next commits.
    
    Cc: Steve French <smfrench@gmail.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: Long Li <longli@microsoft.com>
    Cc: Namjae Jeon <linkinjeon@kernel.org>
    Cc: Hyunchul Lee <hyc.lee@gmail.com>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/common/smb_direct/smb_direct_pdu.h
+++ fs/smb/common/smb_direct/smb_direct_pdu.h
@@ -48,4 +48,11 @@ struct smb_direct_data_transfer {
 	__u8 buffer[];
 } __packed;
 
+/* SMB DIRECT buffer descriptor V1 structure [MS-SMBD] 2.2.3.1 */
+struct smb_direct_buffer_descriptor_v1 {
+	__le64 offset;
+	__le32 token;
+	__le32 length;
+} __packed;
+
 #endif /* __FS_SMB_COMMON_SMB_DIRECT_SMB_DIRECT_PDU_H__ */
