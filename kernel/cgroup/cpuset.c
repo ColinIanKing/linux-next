e8341f83e155f50753b58dbd677a36d4eb9ce503
Frederic Weisbecker <frederic@kernel.org>
2025-05-28 18:19:23 +0200

    cpuset: Propagate cpuset isolation update to workqueue through housekeeping
    Until now, cpuset would propagate isolated partition changes to
    workqueues so that unbound workers get properly reaffined.
    
    Since housekeeping now centralizes, synchronize and propagates isolation
    cpumask changes, perform the work from that subsystem for consolidation
    and consistency purposes.
    
    For simplification purpose, the target function is adapted to take the
    new housekeeping mask instead of the isolated mask.
    
    Suggested-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Reviewed-by: Waiman Long <longman@redhat.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Cc: "Michal Koutný" <mkoutny@suse.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>
    Cc: Marco Crivellari <marco.crivellari@suse.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Waiman Long <longman@redhat.com>
    Cc: cgroups@vger.kernel.org

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1484,13 +1484,10 @@ static void update_isolation_cpumasks(void)
 
 	lockdep_assert_cpus_held();
 
-	ret = workqueue_unbound_exclude_cpumask(isolated_cpus);
+	ret = housekeeping_update(isolated_cpus);
 	WARN_ON_ONCE(ret < 0);
 
 	ret = tmigr_isolated_exclude_cpumask(isolated_cpus);
-	WARN_ON_ONCE(ret < 0);
-
-	ret = housekeeping_update(isolated_cpus);
 	WARN_ON_ONCE(ret < 0);
 
 	isolated_cpus_updating = false;
