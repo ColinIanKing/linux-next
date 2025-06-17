ec1a430184f6e54f387a3039bd590c4a8a4c4f00
Kent Overstreet <kent.overstreet@linux.dev>
2025-06-17 10:52:13 -0400

    bcachefs: More errcode conversions
    Convert standard errcodes to private error codes, and return them with
    bch_err_throw(), for better debugging.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/fs.c
+++ fs/bcachefs/fs.c
@@ -1693,10 +1693,10 @@ static int bch2_fileattr_set(struct mnt_idmap *idmap,
 		s.mask = map_defined(bch_flags_to_xflags);
 		s.flags |= map_flags_rev(bch_flags_to_xflags, fa->fsx_xflags);
 		if (fa->fsx_xflags)
-			return -EOPNOTSUPP;
+			return bch_err_throw(c, unsupported_fsx_flag);
 
 		if (fa->fsx_projid >= U32_MAX)
-			return -EINVAL;
+			return bch_err_throw(c, projid_too_big);
 
 		/*
 		 * inode fields accessible via the xattr interface are stored with a +1
@@ -1719,7 +1719,7 @@ static int bch2_fileattr_set(struct mnt_idmap *idmap,
 
 		s.flags |= map_flags_rev(bch_flags_to_uflags, fa->flags);
 		if (fa->flags)
-			return -EOPNOTSUPP;
+			return bch_err_throw(c, unsupported_fa_flag);
 	}
 
 	mutex_lock(&inode->ei_update_lock);
@@ -2563,7 +2563,7 @@ got_sb:
 
 	sb->s_shrink->seeks = 0;
 
-	if (bch2_fs_casefold_enabled(c))
+	if (!bch2_fs_casefold_enabled(c))
 		sb->s_encoding = c->cf_encoding;
 	generic_set_sb_d_ops(sb);
 
