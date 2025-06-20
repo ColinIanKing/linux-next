c215ccda74dcfd8a989d1f5bea9fded18cc30102
Hao Ge <gehao@kylinos.cn>
2025-06-20 17:31:02 +0800

    mm-alloc_tag-fix-the-kmemleak-false-positive-issue-in-the-allocation-of-the-percpu-variable-tag-counters-v3
    fix build error when CONFIG_DEBUG_KMEMLEAK=n, s/igonore/ignore/
    
    Link: https://lkml.kernel.org/r/20250620093102.2416767-1-hao.ge@linux.dev
    Fixes: 12ca42c23775 ("alloc_tag: allocate percpu counters for module tags dynamically")
    Signed-off-by: Hao Ge <gehao@kylinos.cn>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Kent Overstreet <kent.overstreet@linux.dev>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -1247,18 +1247,18 @@ void __ref kmemleak_transient_leak(const void *ptr)
 EXPORT_SYMBOL(kmemleak_transient_leak);
 
 /**
- * kmemleak_ignore_phys - similar to kmemleak_ignore but taking a percpu
- *			  address argument
+ * kmemleak_ignore_percpu - similar to kmemleak_ignore but taking a percpu
+ *			    address argument
  * @ptr:	percpu address of the object
  */
-void __ref kmemleak_igonore_percpu(const void __percpu *ptr)
+void __ref kmemleak_ignore_percpu(const void __percpu *ptr)
 {
 	pr_debug("%s(0x%px)\n", __func__, ptr);
 
 	if (kmemleak_enabled && ptr && !IS_ERR_PCPU(ptr))
 		make_black_object((unsigned long)ptr, OBJECT_PERCPU);
 }
-EXPORT_SYMBOL_GPL(kmemleak_igonore_percpu);
+EXPORT_SYMBOL_GPL(kmemleak_ignore_percpu);
 
 /**
  * kmemleak_ignore - ignore an allocated object
