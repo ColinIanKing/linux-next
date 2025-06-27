93f02fb8554dbd164d540d99b42bffc8de271b5a
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2025-06-27 23:19:30 +0900

    ocfs2: update d_splice_alias() return code checking
    When commit d3556babd7fa ("ocfs2: fix d_splice_alias() return code
    checking") was merged into v3.18-rc3, d_splice_alias() was returning one
    of a valid dentry, NULL or an ERR_PTR.
    
    When commit b5ae6b15bd73 ("merge d_materialise_unique() into
    d_splice_alias()") was merged into v3.19-rc1, d_splice_alias() started
    returning -ELOOP as one of ERR_PTR values.
    
    Now, when syzkaller mounts a crafted ocfs2 filesystem image that hits
    d_splice_alias() == -ELOOP case from ocfs2_lookup(), ocfs2_lookup() fails
    to handle -ELOOP case and generic_shutdown_super() hits "VFS: Busy inodes
    after unmount" message.
    
    Instead of calling ocfs2_dentry_attach_lock() or ocfs2_dentry_attach_gen()
    when d_splice_alias() returned an ERR_PTR value, change ocfs2_lookup() to
    bail out immediately.
    
    Also, ocfs2_lookup() needs to call dupt() when ocfs2_dentry_attach_lock()
    returned an ERR_PTR value.
    
    Link: https://lkml.kernel.org/r/d84dc916-2982-45dc-a9a5-a6255cbc62bd@I-love.SAKURA.ne.jp
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Reported-by: syzbot <syzbot+1134d3a5b062e9665a7a@syzkaller.appspotmail.com>
    Closes: https://syzkaller.appspot.com/bug?extid=1134d3a5b062e9665a7a
    Suggested-by: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Mark Fasheh <mark@fasheh.com>
    Cc: Joel Becker <jlbec@evilplan.org>
    Cc: Junxiao Bi <junxiao.bi@oracle.com>
    Cc: Joseph Qi <jiangqi903@gmail.com>
    Cc: Changwei Ge <gechangwei@live.cn>
    Cc: Jun Piao <piaojun@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/ocfs2/namei.c
+++ fs/ocfs2/namei.c
@@ -142,6 +142,8 @@ static struct dentry *ocfs2_lookup(struct inode *dir, struct dentry *dentry,
 
 bail_add:
 	ret = d_splice_alias(inode, dentry);
+	if (IS_ERR(ret))
+		goto bail_unlock;
 
 	if (inode) {
 		/*
@@ -154,13 +156,12 @@ bail_add:
 		 * NOTE: This dentry already has ->d_op set from
 		 * ocfs2_get_parent() and ocfs2_get_dentry()
 		 */
-		if (!IS_ERR_OR_NULL(ret))
-			dentry = ret;
-
-		status = ocfs2_dentry_attach_lock(dentry, inode,
+		status = ocfs2_dentry_attach_lock(ret ? ret : dentry, inode,
 						  OCFS2_I(dir)->ip_blkno);
 		if (status) {
 			mlog_errno(status);
+			if (ret)
+				dput(ret);
 			ret = ERR_PTR(status);
 			goto bail_unlock;
 		}
