a1984b2541b064cb5b5bc67048a2f1bf9a7804e5
Radim Krčmář <rkrcmar@ventanamicro.com>
2025-07-25 18:54:11 +0200

    riscv: use TASK_TI_CPU instead of TASK_TI_CPU_NUM
    The offsets of TASK_TI_CPU and TASK_TI_CPU_NUM are identical, and
    TASK_TI_CPU is a better name for thread_info.cpu.
    
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Signed-off-by: Radim Krčmář <rkrcmar@ventanamicro.com>
    Link: https://lore.kernel.org/r/20250725165410.2896641-6-rkrcmar@ventanamicro.com
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

--- arch/riscv/kernel/asm-offsets.c
+++ arch/riscv/kernel/asm-offsets.c
@@ -49,7 +49,6 @@ void asm_offsets(void)
 	OFFSET(TASK_TI_A2, task_struct, thread_info.a2);
 #endif
 
-	OFFSET(TASK_TI_CPU_NUM, task_struct, thread_info.cpu);
 	OFFSET(TASK_THREAD_F0,  task_struct, thread.fstate.f[0]);
 	OFFSET(TASK_THREAD_F1,  task_struct, thread.fstate.f[1]);
 	OFFSET(TASK_THREAD_F2,  task_struct, thread.fstate.f[2]);
