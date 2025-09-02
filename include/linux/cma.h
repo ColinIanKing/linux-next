fd13e7ccaacb19be4f1f278353aa6d1ec055269a
Kefeng Wang <wangkefeng.wang@huawei.com>
2025-09-02 20:48:20 +0800

    mm: hugetlb: allocate frozen pages in alloc_gigantic_folio()
    The alloc_gigantic_folio() will allocate a folio by alloc_contig_range()
    with refcount increated and then freeze it, convert to allocate a frozen
    folio directly to remove the atomic operation about folio refcount, also
    saving atomic operation during __update_and_free_hugetlb_folio too.
    
    Rename some functions to make them more self-explanatory,
    
      folio_alloc_gigantic		-> folio_alloc_frozen_gigantic
      cma_alloc/free_folio		-> cma_alloc/free_frozen_folio
      hugetlb_cma_alloc/free_folio	-> hugetlb_cma_alloc/free_frozen_folio
    
    Link: https://lkml.kernel.org/r/20250902124820.3081488-10-wangkefeng.wang@huawei.com
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Brendan Jackman <jackmanb@google.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Jane Chu <jane.chu@oracle.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Sidhartha Kumar <sidhartha.kumar@oracle.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/cma.h
+++ include/linux/cma.h
@@ -57,16 +57,16 @@ extern bool cma_intersects(struct cma *cma, unsigned long start, unsigned long e
 extern void cma_reserve_pages_on_error(struct cma *cma);
 
 #ifdef CONFIG_CMA
-struct folio *cma_alloc_folio(struct cma *cma, int order, gfp_t gfp);
-bool cma_free_folio(struct cma *cma, const struct folio *folio);
+struct folio *cma_alloc_frozen_folio(struct cma *cma, int order, gfp_t gfp);
+bool cma_free_frozen_folio(struct cma *cma, const struct folio *folio);
 bool cma_validate_zones(struct cma *cma);
 #else
-static inline struct folio *cma_alloc_folio(struct cma *cma, int order, gfp_t gfp)
+static inline struct folio *cma_alloc_frozen_folio(struct cma *cma, int order, gfp_t gfp)
 {
 	return NULL;
 }
 
-static inline bool cma_free_folio(struct cma *cma, const struct folio *folio)
+static inline bool cma_free_frozen_folio(struct cma *cma, const struct folio *folio)
 {
 	return false;
 }
