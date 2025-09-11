47e50b5ff6643045d898dc005e4d9047ab97da6a
Stephen Rothwell <sfr@canb.auug.org.au>
2025-09-11 15:51:25 +1000

    hack for "mm, swap: use the swap table for the swap cache and switch API"
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/swap.h
+++ mm/swap.h
@@ -458,8 +458,10 @@ static inline int non_swapcache_batch(swp_entry_t entry, int max_nr)
 static inline pgoff_t folio_index(struct folio *folio)
 {
 #ifdef CONFIG_MMU
+#ifdef CONFIG_SWAP
 	if (unlikely(folio_test_swapcache(folio)))
 		return swp_offset(folio->swap);
+#endif
 #endif
 	return folio->index;
 }
