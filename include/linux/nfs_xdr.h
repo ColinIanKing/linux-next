af6734d43315feaa1beffdd4d6dfb530497d9387
Mark Brown <broonie@kernel.org>
2025-09-23 15:26:15 +0200

    Revert "NFS: Update listxattr to use xdr_set_scratch_folio()"
    This reverts commit 0349f7e6075933aa8737a97131d230213fe765c9.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/nfs_xdr.h
+++ include/linux/nfs_xdr.h
@@ -1596,7 +1596,7 @@ struct nfs42_listxattrsargs {
 
 struct nfs42_listxattrsres {
 	struct nfs4_sequence_res	seq_res;
-	struct folio			*scratch;
+	struct page			*scratch;
 	void				*xattr_buf;
 	size_t				xattr_len;
 	u64				cookie;
