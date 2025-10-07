32e04a2ca127471ed1ec84208623652a4c56693f
Mike Rapoport (Microsoft) <rppt@kernel.org>
2025-10-07 03:30:56 +0000

    kho: drop notifiers
    The KHO framework uses a notifier chain as the mechanism for clients to
    participate in the finalization process.  While this works for a single,
    central state machine, it is too restrictive for kernel-internal
    components like pstore/reserve_mem or IMA.  These components need a
    simpler, direct way to register their state for preservation (e.g., during
    their initcall) without being part of a complex, shutdown-time notifier
    sequence.  The notifier model forces all participants into a single
    finalization flow and makes direct preservation from an arbitrary context
    difficult.  This patch refactors the client participation model by
    removing the notifier chain and introducing a direct API for managing FDT
    subtrees.
    
    The core kho_finalize() and kho_abort() state machine remains, but clients
    now register their data with KHO beforehand.
    
    Link: https://lkml.kernel.org/r/20251007033100.836886-4-pasha.tatashin@soleen.com
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Masahiro Yamada <masahiroy@kernel.org>
    Cc: Miguel Ojeda <ojeda@kernel.org>
    Cc: Pratyush Yadav <pratyush@kernel.org>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/kexec_handover_internal.h
+++ kernel/kexec_handover_internal.h
@@ -30,7 +30,7 @@ void kho_in_debugfs_init(struct kho_debugfs *dbg, const void *fdt);
 int kho_out_debugfs_init(struct kho_debugfs *dbg);
 int kho_debugfs_fdt_add(struct kho_debugfs *dbg, const char *name,
 			const void *fdt, bool root);
-void kho_debugfs_cleanup(struct kho_debugfs *dbg);
+void kho_debugfs_fdt_remove(struct kho_debugfs *dbg, void *fdt);
 #else
 static inline int kho_debugfs_init(void) { return 0; }
 static inline void kho_in_debugfs_init(struct kho_debugfs *dbg,
@@ -38,7 +38,8 @@ static inline void kho_in_debugfs_init(struct kho_debugfs *dbg,
 static inline int kho_out_debugfs_init(struct kho_debugfs *dbg) { return 0; }
 static inline int kho_debugfs_fdt_add(struct kho_debugfs *dbg, const char *name,
 				      const void *fdt, bool root) { return 0; }
-static inline void kho_debugfs_cleanup(struct kho_debugfs *dbg) {}
+static inline void kho_debugfs_fdt_remove(struct kho_debugfs *dbg,
+					  void *fdt) { }
 #endif /* CONFIG_KEXEC_HANDOVER_DEBUGFS */
 
 #endif /* LINUX_KEXEC_HANDOVER_INTERNAL_H */
