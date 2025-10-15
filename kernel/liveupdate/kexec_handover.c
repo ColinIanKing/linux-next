08ae9c92ff6dd4a0674c0ee9cfd71439f111c7a4
Andrew Morton <akpm@linux-foundation.org>
2025-10-14 19:46:59 -0700

    liveupdate-kho-move-to-kernel-liveupdate-fix
    fix kho_preserve_vmalloc ENOMEM return, per Dan
    
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Masahiro Yamada <masahiroy@kernel.org>
    Cc: Miguel Ojeda <ojeda@kernel.org>
    Cc: "Mike Rapoport (Microsoft)" <rppt@kernel.org>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Pratyush Yadav <pratyush@kernel.org>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Dan Carpenter <dan.carpenter@linaro.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/liveupdate/kexec_handover.c
+++ kernel/liveupdate/kexec_handover.c
@@ -974,8 +974,10 @@ int kho_preserve_vmalloc(void *ptr, struct kho_vmalloc *preservation)
 		chunk->phys[idx++] = phys;
 		if (idx == ARRAY_SIZE(chunk->phys)) {
 			chunk = new_vmalloc_chunk(chunk);
-			if (!chunk)
+			if (!chunk) {
+				err = -ENOMEM;
 				goto err_free;
+			}
 			idx = 0;
 		}
 	}
