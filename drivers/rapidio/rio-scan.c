639a75e1845b3b627138abf50d50736855090944
Haoxiang Li <lihaoxiang@isrc.iscas.ac.cn>
2025-12-17 17:15:48 +0800

    rapidio: replace rio_free_net() with kfree() in rio_scan_alloc_net()
    When idtab allocation fails, net is not registered with rio_add_net() yet,
    so kfree(net) is sufficient to release the memory.
    
    Link: https://lkml.kernel.org/r/20251217091548.482358-1-lihaoxiang@isrc.iscas.ac.cn
    Fixes: e6b585ca6e81 ("rapidio: move net allocation into core code")
    Signed-off-by: Haoxiang Li <lihaoxiang@isrc.iscas.ac.cn>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alexandre Bounine <alex.bou9@gmail.com>
    Cc: Matt Porter <mporter@kernel.crashing.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/rapidio/rio-scan.c
+++ drivers/rapidio/rio-scan.c
@@ -854,7 +854,7 @@ static struct rio_net *rio_scan_alloc_net(struct rio_mport *mport,
 
 		if (idtab == NULL) {
 			pr_err("RIO: failed to allocate destID table\n");
-			rio_free_net(net);
+			kfree(net);
 			net = NULL;
 		} else {
 			net->enum_data = idtab;
