e34e7e075653295019066718950c40117a57b7dc
Qiang Ma <maqianga@uniontech.com>
2025-12-19 17:31:32 +0800

    kexec: add kexec flag to control debug printing
    commit cbc2fe9d9cb2 ("kexec_file: add kexec_file flag to control debug
    printing") added the kexec_file flag to control debug printing.  However,
    for arm64, after commit 6f8c1da071a4 ("kexec_file, arm64: print out
    debugging message if required"), when using the kexec_load interface, the
    kexec_image_info function is called to print debug message, but it uses
    kexec_dprintk, which is only available under kexec_file, then when
    specifying 'kexec -d', kexec_load interface will print nothing.
    
    For riscv, commit eb7622d908a09 ("kexec_file, riscv: print out debugging
    message if required") remove kexec_image_info(), because the content has
    been printed out in generic code in kexec_file_load, but not in
    kexec_load.
    
    Therefore, in order to solve the two problems mentioned above, for this
    patchset, several things need to be done:
    
    1. Enabling kexec_dprintk is available for kexec_load.
    2. Add some debugging message of the deleted kexec_image_info
       to generic code.
    3. Remove duplicate debugging prints for arm64
    
    Since kexec_load and kexec_file_load are not triggered simultaneously,
    we can unify the debug flag of kexec and kexec_file as kexec_dbg_print.
    
    In this way, kexec_dprintk is available for kexec_load and kexec_file_load.
    
    The following are the key points of the specific changes:
    
    1. rename kexec_file_dbg_print to kexec_dbg_print
    2. Add KEXEC_DEBUG
    3. Initialize kexec_dbg_print for kexec
    4. Set the reset of kexec_dbg_print to kimage_free
    
    Link: https://lkml.kernel.org/r/20251219093134.2268620-3-maqianga@uniontech.com
    Signed-off-by: Qiang Ma <maqianga@uniontech.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/kexec_file.c
+++ kernel/kexec_file.c
@@ -138,8 +138,6 @@ void kimage_file_post_load_cleanup(struct kimage *image)
 	 */
 	kfree(image->image_loader_data);
 	image->image_loader_data = NULL;
-
-	kexec_file_dbg_print = false;
 }
 
 #ifdef CONFIG_KEXEC_SIG
@@ -314,7 +312,7 @@ kimage_file_alloc_init(struct kimage **rimage, int kernel_fd,
 	if (!image)
 		return -ENOMEM;
 
-	kexec_file_dbg_print = !!(flags & KEXEC_FILE_DEBUG);
+	kexec_dbg_print = !!(flags & KEXEC_FILE_DEBUG);
 	image->file_mode = 1;
 
 #ifdef CONFIG_CRASH_DUMP
