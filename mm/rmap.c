d5752da0378fab0de12a913b3496cd3f5c76b883
Dev Jain <dev.jain@arm.com>
2026-01-16 13:57:21 +0530

    mm: fix uffd-wp bit loss when batching file folio unmapping
    The recently added file folio unmap batching support forgets to update
    pte_install_uffd_wp_if_needed(), which still updates a single pte.  We end
    up jumping to the end of the folio in page_vma_mapped_walk(), thus setting
    the uffd-wp marker only on a single pte in the batch.  Fix this by passing
    nr_pages into the function, and set the uffd-wp marker on all ptes.
    
    Note that, since the nr_pages passed to this function is always derived by
    some sort of batching, it is guaranteed that the set of old ptevals of the
    batch have uffd-wp bit on all ptes or no ptes, therefore it is safe to
    derive the value of the local variable "arm_uffd_pte" from only the
    particular pteval passed to this function, but apply the result on all
    ptes of the batch.
    
    Use set_pte_at() in a loop to set the markers - we cannot use set_ptes()
    as that will increment the PFN, but we don't have any PFN to update here.
    
    The userspace visible effect of the bug is inaccuracy observed by
    workloads relying on uffd-wp regions to install their own pages.
    
    Link: https://lkml.kernel.org/r/20260116082721.275178-1-dev.jain@arm.com
    Fixes: 8798e255b5ec ("mm: rmap: support batched unmapping for file large folios")
    Signed-off-by: Dev Jain <dev.jain@arm.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Harry Yoo <harry.yoo@oracle.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/rmap.c
+++ mm/rmap.c
@@ -2174,7 +2174,7 @@ static bool try_to_unmap_one(struct folio *folio, struct vm_area_struct *vma,
 		 * we may want to replace a none pte with a marker pte if
 		 * it's file-backed, so we don't lose the tracking info.
 		 */
-		pte_install_uffd_wp_if_needed(vma, address, pvmw.pte, pteval);
+		pte_install_uffd_wp_if_needed(vma, address, pvmw.pte, pteval, nr_pages);
 
 		/* Update high watermark before we lower rss */
 		update_hiwater_rss(mm);
