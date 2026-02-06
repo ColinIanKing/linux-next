074790326541f5c26f5d23d9c826339bc4e8cb65
Ran Xiaokai <ran.xiaokai@zte.com.cn>
2026-02-06 04:31:20 +0000

    kho: fix missing early_memunmap() call in kho_populate()
    kho_populate() returns without calling early_memunmap() on success path,
    this will cause early ioremap virtual address space leak.
    
    Link: https://lkml.kernel.org/r/20260206043121.197564-2-ranxiaokai627@163.com
    Fixes: b50634c5e84a ("kho: cleanup error handling in kho_populate()")
    Signed-off-by: Ran Xiaokai <ran.xiaokai@zte.com.cn>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Pratyush Yadav <pratyush@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/liveupdate/kexec_handover.c
+++ kernel/liveupdate/kexec_handover.c
@@ -1463,6 +1463,7 @@ void __init kho_populate(phys_addr_t fdt_phys, u64 fdt_len,
 	struct kho_scratch *scratch = NULL;
 	phys_addr_t mem_map_phys;
 	void *fdt = NULL;
+	int populated = 0;
 	int err;
 
 	/* Validate the input FDT */
@@ -1529,16 +1530,17 @@ void __init kho_populate(phys_addr_t fdt_phys, u64 fdt_len,
 	kho_in.scratch_phys = scratch_phys;
 	kho_in.mem_map_phys = mem_map_phys;
 	kho_scratch_cnt = scratch_cnt;
-	pr_info("found kexec handover data.\n");
 
-	return;
+	populated = 1;
+	pr_info("found kexec handover data.\n");
 
 err_unmap_scratch:
 	early_memunmap(scratch, scratch_len);
 err_unmap_fdt:
 	early_memunmap(fdt, fdt_len);
 err_report:
-	pr_warn("disabling KHO revival\n");
+	if (!populated)
+		pr_warn("disabling KHO revival\n");
 }
 
 /* Helper functions for kexec_file_load */
